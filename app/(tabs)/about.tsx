5e1a70200173bb50a409c9e52f999a3477e6367f
Artiom <alisin@greenmattersinc.com>
2025-06-08 21:29:40 -0700

    Added working tab layout

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,23 +1,18 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function TabLayout() {
-  return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+export default function About(){
+    return(
+        <View style={styles.container}>
+            <Text>This is my first app.</Text>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+    }
+})
+   
